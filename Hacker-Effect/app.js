860ef8ed47a2281fe9c1046cabb3ac3cbcc7c2da
Md Ajmal <maherukh1999@gmail.com>
2023-03-07 20:48:01 +0530

    completed the haccking game

--- Hacker-Effect/app.js
+++ Hacker-Effect/app.js
@@ -1,6 +1,24 @@
 let h1 = document.querySelector('h1');
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let interval = null;
 
 h1.onmouseover = event => {
-    event.target.innerText = letters[Math.floor(Math.random() * 26)];
+    let iterations = 0;
+    clearInterval(interval);
+     interval = setInterval(() => {
+        event.target.innerText = event.target.innerText.split("")
+        .map((letter, index) => {
+            if(index < iterations) {
+                return event.target.dataset.value[index];
+            }
+            return letters[Math.floor(Math.random() * 26)]
+        })
+        .join("");
+
+        if(iterations >= event.target.dataset.value.length) {
+            clearInterval(interval);
+        }
+
+        iterations += 1 / 3;
+    }, 30);
 }
